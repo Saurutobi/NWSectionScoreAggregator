ffd31225193319fecd22a20b06e037ad7d039354
Marcel Englmaier <saurutobi@gmail.com>
2022-01-30 18:44:38 -0800

    DQ report only exports matches with DQs

--- src/main/java/com/saurutobi/NWSectionScoreAggregator/SectionAggregator.java
+++ src/main/java/com/saurutobi/NWSectionScoreAggregator/SectionAggregator.java
@@ -22,7 +22,11 @@ public class SectionAggregator {
     public static void aggregateMatch(String inputDirectory, String outputFileName, boolean dqOnly) {
         Option.of(inputDirectory).peek(inputDir -> Option.of(outputFileName).peek(outputFile -> {
             final List<Match> matches = readConvertedMatches(inputDir);
-            writeSectionReport(outputFile, matches, getAllDistinctMembersWhoParticipatedByUspsaNumber(matches), dqOnly);
+            if (dqOnly) {
+                writeSectionDqReport(outputFile, matches, getAllDistinctMembersWhoParticipatedByUspsaNumber(matches));
+            } else {
+                writeSectionMatchReport(outputFile, matches, getAllDistinctMembersWhoParticipatedByUspsaNumber(matches));
+            }
         }));
     }
 
@@ -59,7 +63,53 @@ public class SectionAggregator {
         }
     }
 
-    private static void writeSectionReport(String outputFileName, List<Match> matches, List<Tuple3<String, String, String>> participants, boolean dqOnly) {
+    private static void writeSectionDqReport(String outputFileName, List<Match> matches, List<Tuple3<String, String, String>> participants) {
+        try {
+            final FileWriter myWriter = new FileWriter(outputFileName);
+            final List<Match> matchesWithNoDqs = matches.stream()
+                    .filter(SectionAggregator::matchContainsDq)
+                    .collect(Collectors.toList());
+
+            writeSectionReportHeader(myWriter, matchesWithNoDqs);
+            for (Tuple3<String, String, String> participant : participants) {
+                final StringBuilder out = new StringBuilder(participant._1 + DELIMITER + participant._2 + DELIMITER + participant._3);
+                for (Match match : matchesWithNoDqs) {
+                    final Optional<Participant> participantAtMatch = match.participants.stream()
+                            .filter(matchParticipant -> matchParticipant.getUspsaNumber().equals(participant._3))
+                            .findFirst();
+                    if (participantAtMatch.isPresent()) {
+                        final Participant participantForRecord = participantAtMatch.get();
+                        out.append("|")
+                                .append(participantForRecord.isDQed ? "DQ" : "__");
+                    } else {
+                        out.append(DELIMITER)
+                                .append("__");
+                    }
+                }
+                out.append("\n");
+                String printValue = out.toString();
+
+                //Remove those that never DQed
+                if (!printValue.contains("DQ")) {
+                    printValue = "";
+                }
+                System.out.print(printValue);
+                myWriter.write(printValue);
+            }
+            myWriter.flush();
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("error writing file");
+            System.out.println(e);
+        }
+    }
+
+    private static boolean matchContainsDq(Match match) {
+        return match.participants.stream()
+                .anyMatch(participant -> participant.isDQed);
+    }
+
+    private static void writeSectionMatchReport(String outputFileName, List<Match> matches, List<Tuple3<String, String, String>> participants) {
         try {
             final FileWriter myWriter = new FileWriter(outputFileName);
             writeSectionReportHeader(myWriter, matches);
@@ -71,18 +121,14 @@ public class SectionAggregator {
                             .findFirst();
                     if (participantAtMatch.isPresent()) {
                         final Participant participantForRecord = participantAtMatch.get();
-                        if (dqOnly) {
-                            out.append("|")
-                                    .append(participantForRecord.isDQed ? "DQ" : "__");
-                        } else {
-                            out.append("|")
-                                    .append("Division:")
-                                    .append(participantForRecord.getDivision())
-                                    .append(" DivisionFinish:")
-                                    .append(participantForRecord.getDivisonFinish())
-                                    .append(" isQDed:")
-                                    .append(participantForRecord.isDQed);
-                        }
+
+                        out.append("|")
+                                .append("Division:")
+                                .append(participantForRecord.getDivision())
+                                .append(" DivisionFinish:")
+                                .append(participantForRecord.getDivisonFinish())
+                                .append(" isQDed:")
+                                .append(participantForRecord.isDQed);
                     } else {
                         out.append(DELIMITER)
                                 .append("__");
@@ -90,11 +136,6 @@ public class SectionAggregator {
                 }
                 out.append("\n");
                 String printValue = out.toString();
-
-                //Remove those that never DQed
-                if (dqOnly && !printValue.contains("DQ")) {
-                    printValue = "";
-                }
                 System.out.print(printValue);
                 myWriter.write(printValue);
             }
